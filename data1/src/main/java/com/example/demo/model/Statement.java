8cf175b844f99aa133eda4f9d5a16b9d1c320b2b
Eugene <zhenyasmagin@yandex.ru>
2019-09-17 18:25:10 +0300

    add data.sql and statement

---
+++ data1/src/main/java/com/example/demo/model/Statement.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+import com.sun.istack.internal.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class Statement {
+    @NotNull
+    private Long id;
+    @NotNull
+    private Date date;
+    @NotNull
+    private String daterType;
+    @NotNull
+    private Integer value;
+}
+
+
