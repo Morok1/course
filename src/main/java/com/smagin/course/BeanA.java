db0f792fe983c32b479c1e5bd2a3a0a90d7b23da
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-09-29 20:47:20 +0300

    Add config

--- src/main/java/com/smagin/course/BeanA.java
+++ src/main/java/com/smagin/course/BeanA.java
@@ -3,8 +3,6 @@ package com.smagin.course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
 public class BeanA {
-    @Autowired
     private BeanB b;
 }
